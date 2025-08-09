82d0ac1586c2d96521fe47a285543b9d00309389
na-jimenezc <141644016+na-jimenezc@users.noreply.github.com>
2025-08-09 14:28:36 +0000

    Primer commit :D - Inicializacion con springboot

---
+++ animalheart/src/main/java/com/animalheart/animalheart/AnimalheartApplication.java
@@ -0,0 +1,13 @@
+package com.animalheart.animalheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalheartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalheartApplication.class, args);
+	}
+
+}
